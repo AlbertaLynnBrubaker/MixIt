ea105438b3f84c9d3bc16bb8dd71b2dab20ff3fc
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-07-11 15:07:28 -0500

    render drink list, render drink details, CSS styling for both

---
+++ README.md
@@ -0,0 +1,11 @@
+1) Create context menu button event listener to open side menu
+    a) have menu slide open and make list items visible
+
+2) Create event listeners for each search option in sidebar which open the form across the top
+
+3) Create event listener for form to input search term to API
+    a) have fetch retrieve search
+
+4) Append search to main body of page on left half
+
+5) Create event listener on each populated cocktail option to open another more detailed page on the right half of the main body
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const firstLetter = document.querySelector('#first-letter');
 const nameSearchForm = document.querySelector('#search-name');
 const ingredientSearchForm = document.querySelector('#search-ingredient');
 const letterDropdown = document.querySelector('#letter-dropdown');
+const searchForms = document.querySelectorAll('.search');
+const drinksContainer = document.querySelector('#drinks-container');
 const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/';
 const byName = 'search.php?s=';
 const byIngredient = 'filter.php?i=';
@@ -26,6 +28,8 @@ let toggleNav = function() {
         menuButton.style.width = "300px";
         getSidebar.style.width = "290px";
         getSidebarTitle.style.opacity = "0.5";
+        drinksContainer.style.marginLeft = "320px"
+        drinksContainer.style.width = "calc(100% - 320px)"
 
         for (let i = 0; i < getSidebarLinks.length; i++) {
             getSidebarLinks[i].style.opacity = "1";
@@ -36,6 +40,8 @@ let toggleNav = function() {
         getSidebar.style.width = "90px";
         menuButton.style.width = "100px";
         getSidebarTitle.style.opacity = "0";
+        drinksContainer.style.marginLeft = "120px"
+        drinksContainer.style.width = "calc(100% - 120px)"
 
         for (let i = 0; i < getSidebarLinks.length; i++) {
             getSidebarLinks[i].style.opacity = "0";
@@ -47,17 +53,74 @@ let toggleNav = function() {
     }
 }
 
+function renderList(data) {
+    const drinksListContainer = document.querySelector('#drink-list')
+    data.drinks.forEach(drink => {
+        console.log(drink)
+        const drinkItem = document.createElement('p');
+        const drinkThumb = document.createElement('img');
+        drinkThumb.className = "drink-thumb";
+
+        drinkThumb.src = drink.strDrinkThumb;
+        
+        drinkItem.textContent = drink.strDrink;
+        drinkItem.prepend(drinkThumb)
+        drinksListContainer.append(drinkItem);
+
+        drinkItem.addEventListener('click', () => renderDrinkCard(drink))
+    })
+}
+
+function renderDrinkCard(drink) {
+    const detailContainer = document.querySelector('#drink-details')
+    detailContainer.innerHTML = ''
+    const detailName = document.createElement('h3')
+    const detailImage = document.createElement('img')
+    const ingredientsTitle = document.createElement('h4')
+    const detailIngredients = document.createElement('ul')
+    
+    detailImage.className = "detail-image"
+    detailName.textContent = drink.strDrink;
+    detailImage.src = drink.strDrinkThumb;
+
+    ingredientsTitle.textContent = 'Ingredients:'
+    
+    const drinkIngredients = [];
+    for (let i = 1; i < 16 ; i++) {
+        if(drink[`strIngredient${i}`] === null) {
+            break;
+        }
+        drinkIngredients.push(drink[`strIngredient${i}`])
+    }
+    drinkIngredients.forEach(ingredient => {
+        const detailIngredient = document.createElement('li')
+        detailIngredient.textContent = ingredient
+        detailIngredients.append(detailIngredient)
+    })
+
+    detailContainer.append(detailName, detailImage, ingredientsTitle, detailIngredients)
+}
 cocktailName.addEventListener('click', (e) => {
-  ingredientSearchForm.style.display = 'none';
+    searchForms.forEach(searchForm => {
+        searchForm.style.display = 'none'
+    })
+//   ingredientSearchForm.style.display = 'none';
   nameSearchForm.style.display = 'block';
 })
 
 mainIngredient.addEventListener('click', (e) => {
-  nameSearchForm.style.display = 'none';
+    searchForms.forEach(searchForm => {
+        searchForm.style.display = 'none'
+    })
+//   nameSearchForm.style.display = 'none';
   ingredientSearchForm.style.display = 'block';
 })
 
 firstLetter.addEventListener('click', () => {
+    searchForms.forEach(searchForm => {
+        searchForm.style.display = 'none'
+    })
+
   populateLetterDropdown();
   letterDropdown.style.display = 'block';
 })
@@ -70,7 +133,7 @@ nameSearchForm.addEventListener('submit', (e) => {
   const input = e.target.input.value;
 
   fetchRequest(byName, input)
-  .then(data => console.log(data));
+  .then(renderList);
 })
 
 ingredientSearchForm.addEventListener('submit', (e) => {
@@ -79,7 +142,7 @@ ingredientSearchForm.addEventListener('submit', (e) => {
   const input = e.target.input.value;
 
   fetchRequest(byIngredient, input)
-  .then(data => console.log(data));
+  .then(renderList);
 })
 
 function fetchRequest(trailingUrl, input) {
