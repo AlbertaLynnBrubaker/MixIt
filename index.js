14935b3996b5e35d3eb6d359f3b5f1e3ddc28ea1
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-07-11 09:32:34 -0500

    have sidebar menu working

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
-    .then(r => r.json())
-    .then(schedule => {
-        console.log(schedule)
-    })
+let toggleNavStatus = false;
+
+const menuButton = document.querySelector('div.btn-toggle-nav');
+
+console.log(menuButton)
+
+let toggleNav = function() {
+    let getSidebar = document.querySelector(".sidebar");
+    let getSidebarUl = document.querySelector(".sidebar ul");
+    let getSidebarTitle = document.querySelector(".sidebar span");
+    let getSidebarLinks = document.querySelectorAll(".sidebar a");
+
+
+
+    if (toggleNavStatus === false) {
+        getSidebarUl.style.visibility = "visible";
+        menuButton.style.width = "300px";
+        getSidebar.style.width = "290px";
+        getSidebarTitle.style.opacity = "0.5";
+
+        for (let i = 0; i < getSidebarLinks.length; i++) {
+            getSidebarLinks[i].style.opacity = "1";
+        }
+
+        toggleNavStatus = true;
+    } else if (toggleNavStatus === true) {
+        getSidebar.style.width = "90px";
+        menuButton.style.width = "100px";
+        getSidebarTitle.style.opacity = "0";
+
+        for (let i = 0; i < getSidebarLinks.length; i++) {
+            getSidebarLinks[i].style.opacity = "0";
+        }
+
+        getSidebarUl.style.visibility = "hidden";
+
+        toggleNavStatus = false;
+    }
+}
+
+menuButton.addEventListener('click', toggleNav)
